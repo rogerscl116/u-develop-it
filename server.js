737e51325bb48429e71a0b92939e954cc531c76e
charity.rogers <rogerscl116@gmail.com>
2021-06-27 22:17:26 -0500

    build the database calls

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,8 +8,30 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MySQL username
+        user: 'MYSQL80',
+        // your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
 
+//  db.query(`SELECT * FROM candidates`, (err, rows) => {
+//      console.log(rows);
+//    });
 
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(row);
+  });
 
 // default response for any other request (not found)
 app.use((req, res) => {
