bd026c3bbf35356e99a32f67269b2ac4639f8e86
charity.rogers <rogerscl116@gmail.com>
2021-06-30 18:46:24 -0500

    move code to partyRoutes.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const inputCheck = require('./utils/inputCheck');
 const db = require('./db/connection');
-
-// add near the top of the file
 const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
@@ -11,67 +9,20 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// add after express middleware
+
+// use apiRoutes
 app.use('/api', apiRoutes);
-
-// get all parties
-app.get('/api/parties', (req, res) => {
-  const sql = `SELECT * FROM parties`;
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: rows
-    });
-  });
-});
-
-// get single party
-app.get('/api/party/:id', (req, res) => {
-  const sql = `SELECT * FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-  db.query(sql, params, (err, row) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: row
-    });
-  });
-});
-
-// delete a party
-app.delete('/api/party/:id', (req, res) => {
-  const sql = `DELETE FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: res.message });
-      // checks if anything was deleted
-    } else if (!result.affectedRows) {
-      res.json({
-        message: 'Party not found'
-      });
-    } else {
-      res.json({
-        message: 'deleted',
-        changes: result.affectedRows,
-        id: req.params.id
-      });
-    }
-  });
-});
 
 // default response for any other request (not found)
 app.use((req, res) => {
     res.status(404).end();
-})
+});
 
+// start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+  });
+});
